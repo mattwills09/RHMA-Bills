ba1d9be108d7526e3e98808ad29b78401a8421c5
mattwills09 <mattwills09@gmail.com>
2019-06-03 14:45:59 -0400

    update login/signup code

--- server.js
+++ server.js
@@ -3,27 +3,27 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
-// const routes = require("./routes");
-const passport = require("./passport/");
 const userController = require("./controllers/userController");
-// const MongoStore = require("connect-mongo")(session)
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const users = require("./routes/api/user");
 const app = express();
+const routes = require("./routes");
+const passport = require("./passport/");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
-// Middleware
+// Middleware =========================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // app.use(passport.initalize());
 
-// Sessions
-app.use(session({secret: 'fraggle-rock',
-                //  store: new MongoStore({ mongooseConnection: dbConnection }),
-                 resave: false,
-                 saveUnitialized: false}));
-
+// Sessions ============================
+app.use(session({
+  secret: 'fraggle-rock',
+  //store: new MongoStore({ mongooseConnection: dbConnection }),
+  resave: false,
+  saveUnitialized: false}
+));
 
 // app.use(passport.session());
 
@@ -33,27 +33,24 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/user", userController.create);
-
 app.put("/api/user", userController.update);
-
 app.get("/api/user", userController.get);
 
 // (req, res) => {
 //   console.log("user signup");
 //   req.session.username = req.body.username;
-
   // res.json(req.session.username)
 // }
 
-// Static Assets (usually on heroku)
+// Static Assets (usually on heroku) =========
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// API Routes
+// API Routes ================
 app.use("/api/users", users);
 
-// Connect to the Mongo DB
+// Connect to the Mongo DB ===
 mongoose.connect(
   process.env.MONGODB_URI,
   {
@@ -62,13 +59,12 @@ mongoose.connect(
   }
 );
 
-// Send every request to the React app &
-// Define API routes before this runs
+// Send every request to the React app & ==
+// Define API routes before this runs =====
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
